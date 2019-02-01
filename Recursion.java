a268af8afd698e78457a1112e9bc18854230e42e
DarthChurros <tnarayan00@stuy.edu>
2019-01-31 22:43:32 -0500

    did fibonacci

--- Recursion.java
+++ Recursion.java
@@ -13,10 +13,21 @@ public class Recursion {
     return guess;
   }
 
+  public static int fib(int n) {
+    if (n == 0) return 0;
+    return fib(n, 0, 1);
+  }
+
+  private static int fib(int n, int prev1, int prev2) {
+    if (n > 1) {
+      return fib(n-1, prev1 + prev2, prev1);
+    }
+    return prev1 + prev2;
+  }
+
   public static void main(String[] args) {
-    //for (int i = 0; i < 20; i++) {
-      //System.out.println("sqrt("+i+") = "+sqrt(i, 0.001));
-    //}
-    System.out.println("sqrt(2) = "+sqrt(2,0.1));
+    for (int i = 0; i < 45; i++) {
+      System.out.println("fib("+i+") = "+fib(i));
+    }
   }
 }
