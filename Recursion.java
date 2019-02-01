f37a44f9e9e62cc05ca00a8ed0f97c18df525f7c
DarthChurros <tnarayan00@stuy.edu>
2019-01-31 20:42:44 -0500

    fixed percent error

--- Recursion.java
+++ Recursion.java
@@ -1,21 +1,22 @@
 public class Recursion {
-  public static double sqrt(double n) {
-    return sqrt(n, 1.0);
+  public static double sqrt(double n, double tolerance) {
+    return sqrt(n, tolerance/100, 1.0);
   }
 
-  private static double sqrt(double n, double guess) {
+  private static double sqrt(double n, double tol, double guess) {
     if (n == 0) {
       return 0.0;
     }
-    if (Math.abs(guess * guess - n) > 0.000001) {
-      return sqrt(n, (n / guess + guess) / 2);
+    if (Math.abs(guess * guess - n) > tol*tol) {
+      return sqrt(n, tol, (n / guess + guess) / 2);
     }
-    return Math.round(guess*1000)/1000.0;
+    return guess;
   }
 
   public static void main(String[] args) {
-    for (int i = 0; i < 20; i++) {
-      System.out.println("sqrt("+i+") = "+sqrt(i));
-    }
+    //for (int i = 0; i < 20; i++) {
+      //System.out.println("sqrt("+i+") = "+sqrt(i, 0.001));
+    //}
+    System.out.println("sqrt(2) = "+sqrt(2,0.1));
   }
 }
